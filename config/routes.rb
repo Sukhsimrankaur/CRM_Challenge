c8ae0766d5325ecbb14aec678d17c039d7a1f596
Sukhsimran kaur <sukhsimrankaur@academic.rrc.ca>
2025-07-09 07:48:33 -0500

    Add customers controller, actions, views, routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-  get "customers/index"
-  get "customers/alphabetized"
-  get "customers/missing_email"
+  # Root route
+  root "customers#index"
+
+  # Custom customer routes
+  get "customers/alphabetized", to: "customers#alphabetized", as: "alphabetized_customers"
+  get "customers/missing_email", to: "customers#missing_email", as: "missing_email_customers"
+
+  # ActiveAdmin and Devise
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
